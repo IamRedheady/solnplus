934004b45c2bc3e0a8245939355290357c359401
Redheady <iam@redheady.ru>
2022-04-28 22:16:23 +0300

    404

--- src/js/animation.js
+++ src/js/animation.js
@@ -2,129 +2,132 @@
 import { gsap } from "gsap";
 // Fix safari scroll
 import smoothscroll from 'smoothscroll-polyfill';
-smoothscroll.polyfill();
+const page = document.querySelector(".page")
+if (page) {
+    smoothscroll.polyfill();
 
-gsap.to(".preloader", {
-    y: "-100%",
-    scaleX: 4,
-    duration: 3,
-    ease: "power2.inOut",
-    onComplete: () => {
-        const body = document.querySelector("body")
-        const introContainer = document.querySelector(".intro__container")
-        const header = document.querySelector(".header__container")
-        body.classList.remove("scroll-off")
-        introContainer.classList.add("fade-in")
-        header.classList.add("fade-in-top")
-        setTimeout(() => {
-            header.classList.remove("fade-in-top")
-            header.classList.remove("header-off")
-        }, 1000)
-    }
-})
-
-// Charming
-const charming = require('charming')
-const textItems = document.querySelectorAll('.js-charming-text')
-textItems.forEach(item => {
-    charming(item)
-})
-
-// Плавная анимация элементов
-let isScrolling = false;
-
-window.addEventListener("scroll", throttleScroll, false);
-
-function throttleScroll(e) {
-    if (isScrolling == false) {
-        window.requestAnimationFrame(function () {
-            scrolling(e);
-            isScrolling = false;
-        });
-    }
-    isScrolling = true;
-}
-
-document.addEventListener("DOMContentLoaded", scrolling, false);
-
-// Элементы
-const page = document.querySelector("body")
-const images = document.querySelectorAll(".js-pic-animation")
-const directionsSection = document.querySelector('.directions')
-const worksSection = document.querySelector(".works")
-const plusSection = document.querySelector(".plus")
-function scrolling(e) {
-    textItems.forEach(word => {
-        if (isPartiallyVisible(word)) {
-            const letters = word.childNodes
-            letters.forEach((letter, index) => {
-                setTimeout(() => {
-                    letter.classList.add("fade-in-bottom")
-                }, index * 40)
-            })
-        }
-    })
-    images.forEach(image => {
-        if (isPartiallyVisible(image)) {
-            image.classList.add("fade-in-top")
+    gsap.to(".preloader", {
+        y: "-100%",
+        scaleX: 4,
+        duration: 3,
+        ease: "power2.inOut",
+        onComplete: () => {
+            const body = document.querySelector("body")
+            const introContainer = document.querySelector(".intro__container")
+            const header = document.querySelector(".header__container")
+            body.classList.remove("scroll-off")
+            introContainer.classList.add("fade-in")
+            header.classList.add("fade-in-top")
+            setTimeout(() => {
+                header.classList.remove("fade-in-top")
+                header.classList.remove("header-off")
+            }, 1000)
         }
     })
 
-    if (isPartiallyVisible(directionsSection)) {
-        page.classList.add("dark")
-        directionsSection.classList.add("invert-off")
-        worksSection.classList.add("invert-on")
-    } else {
-        page.classList.remove("dark")
-        directionsSection.classList.remove("invert-off")
-        worksSection.classList.remove("invert-on")
+    // Charming
+    const charming = require('charming')
+    const textItems = document.querySelectorAll('.js-charming-text')
+    textItems.forEach(item => {
+        charming(item)
+    })
+
+    // Плавная анимация элементов
+    let isScrolling = false;
+
+    window.addEventListener("scroll", throttleScroll, false);
+
+    function throttleScroll(e) {
+        if (isScrolling == false) {
+            window.requestAnimationFrame(function () {
+                scrolling(e);
+                isScrolling = false;
+            });
+        }
+        isScrolling = true;
     }
-    if (isPartiallyVisible(plusSection)) {
-        page.classList.remove("dark")
-        directionsSection.classList.remove("invert-off")
-        worksSection.classList.remove("invert-on")
+
+    document.addEventListener("DOMContentLoaded", scrolling, false);
+
+    // Элементы
+    const page = document.querySelector("body")
+    const images = document.querySelectorAll(".js-pic-animation")
+    const directionsSection = document.querySelector('.directions')
+    const worksSection = document.querySelector(".works")
+    const plusSection = document.querySelector(".plus")
+    function scrolling(e) {
+        textItems.forEach(word => {
+            if (isPartiallyVisible(word)) {
+                const letters = word.childNodes
+                letters.forEach((letter, index) => {
+                    setTimeout(() => {
+                        letter.classList.add("fade-in-bottom")
+                    }, index * 40)
+                })
+            }
+        })
+        images.forEach(image => {
+            if (isPartiallyVisible(image)) {
+                image.classList.add("fade-in-top")
+            }
+        })
+
+        if (isPartiallyVisible(directionsSection)) {
+            page.classList.add("dark")
+            directionsSection.classList.add("invert-off")
+            worksSection.classList.add("invert-on")
+        } else {
+            page.classList.remove("dark")
+            directionsSection.classList.remove("invert-off")
+            worksSection.classList.remove("invert-on")
+        }
+        if (isPartiallyVisible(plusSection)) {
+            page.classList.remove("dark")
+            directionsSection.classList.remove("invert-off")
+            worksSection.classList.remove("invert-on")
+        }
     }
-}
 
-function isPartiallyVisible(el) {
-    var elementBoundary = el.getBoundingClientRect();
+    function isPartiallyVisible(el) {
+        var elementBoundary = el.getBoundingClientRect();
 
-    var top = elementBoundary.top;
-    var bottom = elementBoundary.bottom;
-    var height = elementBoundary.height;
+        var top = elementBoundary.top;
+        var bottom = elementBoundary.bottom;
+        var height = elementBoundary.height;
 
-    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
-}
+        return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+    }
 
-function isFullyVisible(el) {
-    var elementBoundary = el.getBoundingClientRect();
+    function isFullyVisible(el) {
+        var elementBoundary = el.getBoundingClientRect();
 
-    var top = elementBoundary.top;
-    var bottom = elementBoundary.bottom;
+        var top = elementBoundary.top;
+        var bottom = elementBoundary.bottom;
 
-    return ((top >= 0) && (bottom <= window.innerHeight));
-}
+        return ((top >= 0) && (bottom <= window.innerHeight));
+    }
 
-// Плавный скролл для страницы. Предустановка npm install smoothscroll-polyfill для safari
-document.querySelectorAll('a[href^="#"').forEach(link => {
+    // Плавный скролл для страницы. Предустановка npm install smoothscroll-polyfill для safari
+    document.querySelectorAll('a[href^="#"').forEach(link => {
 
-    link.addEventListener('click', function (e) {
-        document.querySelector('.header').classList.remove('header__active')
-        document.querySelector("body").classList.remove("scroll-off")
-        e.preventDefault();
+        link.addEventListener('click', function (e) {
+            document.querySelector('.header').classList.remove('header__active')
+            document.querySelector("body").classList.remove("scroll-off")
+            e.preventDefault();
 
-        let href = this.getAttribute('href').substring(1);
+            let href = this.getAttribute('href').substring(1);
 
-        const scrollTarget = document.getElementById(href);
+            const scrollTarget = document.getElementById(href);
 
-        // const topOffset = document.querySelector('.scrollto').offsetHeight;
-        const topOffset = 100; // если не нужен отступ сверху, либо число в px
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
+            // const topOffset = document.querySelector('.scrollto').offsetHeight;
+            const topOffset = 100; // если не нужен отступ сверху, либо число в px
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - topOffset;
 
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth'
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
         });
     });
-});
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,33 @@
 import '../scss/app.scss';
 import "./animation";
-
 /* Your JS Code goes here */
 
+const page = document.querySelector(".page")
+if (page) {
+    // Шапка
+    const menu = document.querySelector('.header');
+    const headerToggleBtn = document.querySelector('.header__toggle');
+    const body = document.querySelector("body")
 
-// Шапка
-const menu = document.querySelector('.header');
-const headerToggleBtn = document.querySelector('.header__toggle');
-const body = document.querySelector("body")
+    headerToggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('header__active')
+        body.classList.toggle("scroll-off")
+    });
 
-headerToggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('header__active')
-    body.classList.toggle("scroll-off")
-});
+    // Попапы
+    const dPopups = document.querySelectorAll(".js-d-popup")
+    const dToggles = document.querySelectorAll(".js-d-popup-toggle")
 
-// Попапы
-const dPopups = document.querySelectorAll(".js-d-popup")
-const dToggles = document.querySelectorAll(".js-d-popup-toggle")
-
-dToggles.forEach(btn => {
-    btn.addEventListener("click", () => {
-        dPopups.forEach(popup => {
-            if (popup.dataset.name === btn.dataset.name) {
-                document.querySelector("body").classList.toggle("scroll-off")
-                popup.classList.toggle("d-popup_on")
-            }
+    dToggles.forEach(btn => {
+        btn.addEventListener("click", () => {
+            dPopups.forEach(popup => {
+                if (popup.dataset.name === btn.dataset.name) {
+                    document.querySelector("body").classList.toggle("scroll-off")
+                    popup.classList.toggle("d-popup_on")
+                }
+            })
         })
     })
-})
 
+
+}
